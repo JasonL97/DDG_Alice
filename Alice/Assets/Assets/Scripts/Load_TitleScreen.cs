4debd7974d5105abf1c4e5a0d6b00aaaa85df2fc
Keefe Neo <33240350+Aerophros@users.noreply.github.com>
2017-11-28 15:35:19 +0800

    Front-End & Buttons

--- Alice/Assets/Assets/Scripts/Load_TitleScreen.cs
+++ Alice/Assets/Assets/Scripts/Load_TitleScreen.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Load_Controls : MonoBehaviour {
+public class Load_TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
 
     public void OnMouseButton()
     {
-        SceneManager.LoadScene("controls");
+        SceneManager.LoadScene("title_screen");
     }
 }
---
+++ Alice/Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed = 5.0f;
+    public float drag = 0.5f;
+    public float terminalRotationSpeed = 25.0f;
+    public Vector3 MoveVector { set; get; }
+    public VirtualJoystick joystick;
+    public Rigidbody thisRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        thisRigidbody = gameObject.GetComponent<Rigidbody>();
+        thisRigidbody.maxAngularVelocity = terminalRotationSpeed;
+        thisRigidbody.drag = drag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveVector = PoolInput();
+        Move();
+		
+	}
+     void Move()
+    {
+        thisRigidbody.AddForce((MoveVector * moveSpeed));
+
+    }
+     Vector3 PoolInput()
+    {
+        Vector3 dir = Vector3.zero;
+        //dir.x = Input.GetAxis("Horizontal");
+        //dir.z = Input.GetAxis("Vertical");
+        dir.x = joystick.Horizontal();
+        dir.z = joystick.Vertical();
+
+        if (dir.magnitude > 1)
+            dir.Normalize();
+        return dir;
+    }
+}
